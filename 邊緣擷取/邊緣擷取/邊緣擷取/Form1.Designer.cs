2930344d6d11ad5ee60704ed6879698e5141431d
hach5412584 <97041113+hach5412584@users.noreply.github.com>
2025-03-04 03:54:18 +0800

    Add files via upload

---
+++ 邊緣擷取/邊緣擷取/邊緣擷取/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace 邊緣擷取
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(143, 585);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "載入影像並灰階";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(534, 567);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(565, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(534, 567);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(696, 585);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 56);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Soble";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(951, 629);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "門檻值(0~255)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(940, 604);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1139, 670);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 邊緣擷取/邊緣擷取/邊緣擷取/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace 邊緣擷取
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openfiledialog = new OpenFileDialog();
+                openfiledialog.Filter = "圖像文件(JPge, Gif, Bmp, etc.)|*.jpg;*.jpge;*.gif;*.bmp;*.tif;*.tiff;*.png;|所有文件(*.*)|*.*";
+                if (openfiledialog.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap MyBitmap = new Bitmap(openfiledialog.FileName);
+                    pictureBox1.Image = MyBitmap;
+                }
+                int height = pictureBox1.Image.Height;
+                int width = pictureBox1.Image.Width;
+                Bitmap newBitmap = new Bitmap(width, height);
+                Bitmap oldBitmap = (Bitmap)pictureBox1.Image;
+                Color pixel;
+                for (int x = 0; x < width; x++)
+                {
+                    for (int y = 0; y < height; y++)
+                    {
+                        pixel = oldBitmap.GetPixel(x, y);
+                        int r, g, b, Result = 0;
+                        r = pixel.R;
+                        b = pixel.B;
+                        g = pixel.G;
+                        Result = (299 * r + 587 * g + 114 * b) / 1000;
+                        newBitmap.SetPixel(x, y, Color.FromArgb(Result, Result, Result));
+                    }
+                }
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox1.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] horizontal = new int[] { 1, 0, -1, 2, 0, -2, 1, 0, -1 };
+                int[] vertical = new int[] { 1, 2, 1, 0, 0, 0, -1, -2, -1 };
+                int height = pictureBox1.Image.Height;
+                int width = pictureBox1.Image.Width;
+                int[,] pixS = new int[height, width];
+                int[] pixel_mask = new int[9];
+                Bitmap newBitmap = new Bitmap(width, height);
+                Bitmap oldBitmap = (Bitmap)pictureBox1.Image;
+                int pixS_h = 0, pixS_v = 0;
+                int max = 0;
+                int min = 0;
+                for (int x = 1; x < width - 1; x++)
+                {
+                    for (int y = 1; y < height - 1; y++)
+                    {
+                        pixel_mask[0] = oldBitmap.GetPixel(x - 1, y - 1).G;
+                        pixel_mask[1] = oldBitmap.GetPixel(x, y - 1).G;
+                        pixel_mask[2] = oldBitmap.GetPixel(x + 1, y - 1).G;
+                        pixel_mask[3] = oldBitmap.GetPixel(x - 1, y).G;
+                        pixel_mask[4] = oldBitmap.GetPixel(x, y).G;
+                        pixel_mask[5] = oldBitmap.GetPixel(x + 1, y).G;
+                        pixel_mask[6] = oldBitmap.GetPixel(x - 1, y + 1).G;
+                        pixel_mask[7] = oldBitmap.GetPixel(x, y + 1).G;
+                        pixel_mask[8] = oldBitmap.GetPixel(x + 1, y + 1).G;
+                        for (int i = 0; i < 9; i++)
+                        {
+                            pixS_h += (pixel_mask[i] * horizontal[i]);
+                            pixS_v += (pixel_mask[i] * vertical[i]);
+                        }
+                        pixS_h = pixS_h / 9;
+                        pixS_v = pixS_v / 9;
+                        pixS_v = Math.Abs(pixS_v);
+                        pixS_h = Math.Abs(pixS_h);
+                        pixS[y-1,x-1] = pixS_h + pixS_v;
+                        if (max < pixS[y - 1, x - 1])
+                        {
+                            max = pixS[y - 1, x - 1];
+                        } 
+                        if (min > pixS[y - 1, x - 1])
+                        {
+                            min = pixS[y - 1, x - 1];
+                        }
+                    }
+                }
+                for (int x = 0; x < width ; x++)
+                {
+                    for (int y = 0; y < height ; y++)
+                    {
+                        pixS[y, x] = (pixS[y, x] - min) * 255 / (max - min);
+                        if (pixS[y, x] >= Convert.ToInt32(textBox1.Text))
+                        {
+                            pixS[y, x] = 255;
+                        }
+                        else
+                        {
+                            pixS[y, x] = 0;
+                        }
+                        newBitmap.SetPixel(x, y, Color.FromArgb(pixS[y, x], pixS[y, x], pixS[y, x]));
+                    }
+                }
+                pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox2.Image = newBitmap;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "訊息提示");
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
